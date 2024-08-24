818ca7918160bf0d5c17dbf263353a44a10beef0
John Kitchin <jkitchin@andrew.cmu.edu>
2024-08-24 13:41:51 +0100

    add rgb form with camera

--- claude/app.py
+++ claude/app.py
@@ -14,13 +14,22 @@ sensor = AS7341(i2c)
 
 app = Flask(__name__)
 
+import io
+from picamera2 import Picamera2
+pc = Picamera2()
 
+    
 def measure(R, G, B):
+    """Perform a measurement at the RGB settings. Returns a dictionary of data
+    that includes the measurements, inputs, time, elapsed time and remote ip
+    address.
+
+    """
     t0 = time.time()
     led = RGBLED(red=19, green=18, blue=12)
     led.color = (R, G, B)
     
-    data = sensor.all_channels
+    data = list(sensor.all_channels) + [sensor.channel_clear, sensor.channel_nir]
     
     results = {        
         'in': [R, G, B],
@@ -83,9 +92,65 @@ def greenmachine1():
     return render_template("green-machine1.html",                           
                            data=())
 
+
+@app.route('/rgb', methods=['GET', 'POST'])
+def rgbmachine():
+    """This is a form for browser use. It is not the most secure, and has none
+    of the WTFform protections. It is unclear how much that matters.
+    """
+    
+    if request.method == 'POST':        
+        R = min(max(float(request.form['R'] or 0), 0.0), 1.0)
+        G = min(max(float(request.form['G'] or 0), 0.0), 1.0)
+        B = min(max(float(request.form['B'] or 0), 0.0), 1.0)
+
+        out = measure(R, G, B)['out']
+        keys = ['R', 'G', 'B'] + list(out.keys())
+        vals = [R, G, B] + list(out.values())
+        
+        csv = '\n'.join([','.join([str(x) for x in row]) for row in zip(keys, vals)])
+        b64 = base64.b64encode(csv.encode('utf-8')).decode("utf8")
+
+        # Get a picture
+        led = RGBLED(red=19, green=18, blue=12)
+        led.color = (R, G, B)
+    
+
+        pc.start()
+        data = io.BytesIO()
+        pc.capture_file(data, format='png')
+        imgb64 = base64.b64encode(data.getvalue()).decode('utf-8')
+    
+
+        led.color = (0, 0, 0)
+        # I explicitly close so that it is clean to restart. Otherwise, it
+        # seems like the gpio ports are in use and you have to
+        # reboot. there might be some cli way to clear it, but I don't
+        # know what it is.
+        led.close()
+        
+        return render_template("rgb.html",
+                               keys=keys, vals=vals,
+                               imgb64=imgb64,
+                               RGB=(R,G,B),
+                               b64=b64)
+    # this is from GET
+    return render_template("rgb.html",                           
+                           data=())
+
+
 @app.route('/about')
 def about():
     return render_template("about.html")
+
+
+@app.route('/img')
+def img():
+    pc.start()
+    data = io.BytesIO()
+    pc.capture_file(data, format='png')
+    b64 = base64.b64encode(data.getvalue()).decode('utf-8')
+    return f'<img src="data:image/png;base64, {b64}">'
 
 
 def run():
