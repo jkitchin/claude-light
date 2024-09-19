73eca233c67c15e0ec70b2d0659ebf7d6500c12e
John Kitchin <jkitchin@andrew.cmu.edu>
2024-09-19 13:50:48 -0400

    add manual gm

--- gm.py
+++ gm.py
@@ -1,7 +1,22 @@
 import time
 import jsonlines
-from claude_light import GreenMachine1
+import requests
 
+CLAUDE_IP = 'https://claude-light.cheme.cmu.edu/api'
+
+class GreenMachine1:
+    """One input -> one output instrument.
+    """
+    @retry(tries=3, delay=2)
+    def __call__(self, G=0):
+        """Run the instrument.
+        G : int, setting for the green LED channel.
+        Returns the 515nm channel intensity.
+        """
+        resp = requests.get(CLAUDE_IP, params={'R': 0, 'G': G, 'B': 0})
+        data = resp.json()
+        return data['out']['515nm']
+    
 gm = GreenMachine1()
 
 with open('gm.jsonl', 'a') as f:
