c9fac331f17517022d3b3dd49583f1d5ffb0a8ed
John Kitchin <jkitchin@andrew.cmu.edu>
2024-09-19 15:18:23 -0400

    add mod

--- gm.py
+++ gm.py
@@ -34,11 +34,11 @@ fig, p1 = plt.subplots(1, 3)
 with jsonlines.open('gm.jsonl', 'r') as f:
     for line in f:
         if line['G'] == 0.0:
-            p1[0].plot(line['time'], line['result'], 'g.')            
+            p1[0].plot(line['time'] % 3600, line['result'], 'g.')            
         elif line['G'] == 0.5:
-            p1[1].plot(line['time'], line['result'], 'g.')
+            p1[1].plot(line['time'] % 3600, line['result'], 'g.')
         else:
-            p1[2].plot(line['time'], line['result'], 'g.')
+            p1[2].plot(line['time'] % 3600, line['result'], 'g.')
 
 p1[0].set_title('G=0 vs time')
 p1[1].set_title('G=0.5 vs time')
