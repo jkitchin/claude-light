3e7aec85f11a8bb01f6e1b308cdad2fee2ed7ff6
John Kitchin <jkitchin@andrew.cmu.edu>
2024-09-19 14:02:52 -0400

    add linefeed

--- gm.py
+++ gm.py
@@ -23,4 +23,4 @@ with open('gm.jsonl', 'a') as f:
     for g in [0, 0.5, 1.0]:
         for i in range(5):        
             d = {'G': g, 'result': gm(g), 'time': time.time()}
-            f.write(json.dumps(d))
+            f.write(json.dumps(d) + '\n')
